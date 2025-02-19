8ea17479c37f9d409332cc7f678e83a405f3a568
ViktorT777CT <p_viktor91@mail.ru>
2025-02-19 17:39:50 +0600

    vite css

--- example-app/vite.config.js
+++ example-app/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css',
                 'resources/css/desktop/index.css',
+                'resources/css/desktop/end-test.css',
+                'resources/css/desktop/test-a.css',
+                'resources/css/desktop/test-page.css',
                 'resources/js/app.js'],
             refresh: true,
         }),
